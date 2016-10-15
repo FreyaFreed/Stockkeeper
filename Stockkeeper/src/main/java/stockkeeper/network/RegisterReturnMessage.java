d940a4ae22e3fa53d3cf7bde3683802dd83b1a70
FreyaFreed <feminazgul1@gmail.com>
2016-10-15 03:54:43 +0200

    Added some new return messages so client can stop getting errors.

--- Stockkeeper/src/main/java/stockkeeper/network/RegisterReturnMessage.java
+++ Stockkeeper/src/main/java/stockkeeper/network/RegisterReturnMessage.java
@@ -2,14 +2,14 @@ package stockkeeper.network;
 
 public class RegisterReturnMessage extends StockkeeperReturnMessage {
 
-	String registrationMessage;
-	boolean success;
 	public RegisterReturnMessage(String registrationMessage, boolean success) {
 		super(MessageType.REGISTER);
 		this.registrationMessage = registrationMessage;
-		this.success = success;
-
+		this.success = success;		
+		
 	}
-
+	String registrationMessage;
+	boolean success;
+	
 
 }
